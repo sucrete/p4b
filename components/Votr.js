6c25c38ea932a086b51dea20b1d6c1f549a9a1a9
sucrete <williamandree@gmail.com>
2022-10-25 00:39:19 -0500

    plenty to contemplate. GET THIS SHIT DONE

--- components/Votr.js
+++ components/Votr.js
@@ -1,22 +1,21 @@
 const slides = [
     <img src="/votrlogo.png" />,
-    // <img src="/voterbadge.png" />,
-    <img src="votergraphic.png" />,
-  ];
+    <img src="votergraphic2.png" />,
+];
 
 const imageSlides = slides.map((slide, index) => {
     return <li key={`imagesforCal--${index}`} className={`liImage liImage--${index}`}>{slide}</li>;
 });
 
-  export default function Votr() {
+export default function Votr() {
     return (
-      <>
-        <div className="pane votr">
-            <div className="imagesContainer">
-              <ul>{imageSlides}</ul>
+        <>
+            <div className="pane votr">
+                <div className="imagesContainer">
+                    <ul>{imageSlides}</ul>
+                </div>
+                <span className="caption">Assets for professional southwestern person and singer Calvin Todd</span>
             </div>
-            <span className="caption">Assets for professional southwestern person and singer Calvin Todd</span>
-        </div>
-      </>
+        </>
     );
-  }
+}
